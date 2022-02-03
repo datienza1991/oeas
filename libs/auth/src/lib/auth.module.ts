ce812a6e74d01e817231d5756162cfb0e13d54c4
datienza1991 <datienza@doehle-mnl.com>
2022-02-03 22:18:08 +0800

    add Initial forgot password form

--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -14,9 +14,15 @@ import * as fromAuth from './+state/auth.reducer';
 import { AuthEffects } from './+state/auth.effects';
 import { LocalStorageService } from './services/local-storage.service';
 import { storageMetaReducer } from './storage-metareducer';
-import { AUTH_CONFIG_TOKEN, AUTH_LOCAL_STORAGE_KEY, AUTH_STORAGE_KEYS } from './auth.tokens';
+import {
+  AUTH_CONFIG_TOKEN,
+  AUTH_LOCAL_STORAGE_KEY,
+  AUTH_STORAGE_KEYS,
+} from './auth.tokens';
 import { RegisterFormComponent } from './components/register-form/register-form.component';
 import { RegisterComponent } from './containers/register/register.component';
+import { ForgotPasswordComponent } from './containers/forgot-password/forgot-password.component';
+import { ForgotPasswordFormComponent } from './components/forgot-password-form/forgot-password-form.component';
 
 export function getAuthConfig(
   saveKeys: string[],
@@ -30,13 +36,19 @@ export function getAuthConfig(
   };
 }
 
-
 export const authRoutes: Route[] = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'forgot-password', component: LoginComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
 ];
-const COMPONENTS = [LoginComponent, LoginFormComponent, RegisterComponent, RegisterFormComponent];
+const COMPONENTS = [
+  LoginComponent,
+  LoginFormComponent,
+  RegisterComponent,
+  RegisterFormComponent,
+  ForgotPasswordComponent,
+  ForgotPasswordFormComponent,
+];
 @NgModule({
   imports: [
     CommonModule,
@@ -45,7 +57,11 @@ const COMPONENTS = [LoginComponent, LoginFormComponent, RegisterComponent, Regis
     NgZorroAntdModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
-    StoreModule.forFeature(fromAuth.AUTH_FEATURE_KEY, fromAuth.reducer, AUTH_CONFIG_TOKEN),
+    StoreModule.forFeature(
+      fromAuth.AUTH_FEATURE_KEY,
+      fromAuth.reducer,
+      AUTH_CONFIG_TOKEN
+    ),
     EffectsModule.forFeature([AuthEffects]),
   ],
   declarations: COMPONENTS,
@@ -55,11 +71,7 @@ const COMPONENTS = [LoginComponent, LoginFormComponent, RegisterComponent, Regis
     { provide: AUTH_STORAGE_KEYS, useValue: ['user'] },
     {
       provide: AUTH_CONFIG_TOKEN,
-      deps: [
-        AUTH_STORAGE_KEYS,
-        AUTH_LOCAL_STORAGE_KEY,
-        LocalStorageService,
-      ],
+      deps: [AUTH_STORAGE_KEYS, AUTH_LOCAL_STORAGE_KEY, LocalStorageService],
       useFactory: getAuthConfig,
     },
     {
