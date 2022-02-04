4f46ef0d3beecbb3aa8556d164323dde2911d3a5
datienza1991 <datienza@doehle-mnl.com>
2022-02-04 14:22:27 +0800

    update profile form

--- libs/account/src/lib/account.module.ts
+++ libs/account/src/lib/account.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ProfileFormComponent } from './components/profile-form/profile-form.component';
 import { ProfileComponent } from './containers/profile/profile.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgZorroAntdModule } from '@batstateu/ng-zorro-antd';
 
 @NgModule({
   imports: [
     CommonModule,
-
+    ReactiveFormsModule,
+    NgZorroAntdModule,
     RouterModule.forChild([
-      /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
+      { path: 'profile', component: ProfileComponent },
     ]),
   ],
   declarations: [
