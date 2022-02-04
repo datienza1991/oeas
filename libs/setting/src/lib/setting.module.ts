e0908c6ef671c8f3b71580674050d9b609f71199
datienza1991 <datienza@doehle-mnl.com>
2022-02-04 11:06:52 +0800

    update setting

--- libs/setting/src/lib/setting.module.ts
+++ libs/setting/src/lib/setting.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ProfileFormComponent } from './components/profile-form/profile-form.component';
+import { ProfileComponent } from './containers/profile/profile.component';
+import { NgZorroAntdModule } from '@batstateu/ng-zorro-antd';
 
 @NgModule({
   imports: [
     CommonModule,
-
+    NgZorroAntdModule,
     RouterModule.forChild([
-      /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
+      { path: 'profile', component: ProfileComponent },
     ]),
+  ],
+  declarations: [
+    ProfileFormComponent,
+    ProfileComponent
   ],
 })
 export class SettingModule {}
