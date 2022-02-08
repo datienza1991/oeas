005a4683dcc44c20cdeb6a4aa17f723eec94123a
datienza1991 <datienza@doehle-mnl.com>
2022-02-08 21:33:10 +0800

    asdf

--- libs/exams/src/lib/components/exam-recording-view/exam-recording-view.component.ts
+++ libs/exams/src/lib/components/exam-recording-view/exam-recording-view.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ElementRef } from '@angular/core';
+import videojs from 'video.js';
+import * as adapter from 'webrtc-adapter/out/adapter_no_global.js';
+import * as RecordRTC from 'recordrtc';
 
 @Component({
   selector: 'batstateu-exam-recording-view',
   templateUrl: './exam-recording-view.component.html',
-  styleUrls: ['./exam-recording-view.component.less']
+  styleUrls: ['./exam-recording-view.component.less'],
 })
 export class ExamRecordingViewComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
