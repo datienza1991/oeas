ca6d084039915491d657c8e55de2604ac0b95ec0
datienza1991 <datienza@doehle-mnl.com>
2022-02-10 12:15:57 +0800

    Add Change password ui

--- libs/account/src/lib/components/change-password-form/change-password-form.component.ts
+++ libs/account/src/lib/components/change-password-form/change-password-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { NzFormTooltipIcon } from 'ng-zorro-antd/form';
+import { NzModalService } from 'ng-zorro-antd/modal';
 
 @Component({
   selector: 'batstateu-change-password-form',
@@ -7,9 +10,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangePasswordFormComponent implements OnInit {
 
-  constructor() { }
+  
+  validateForm!: FormGroup;
+  captchaTooltipIcon: NzFormTooltipIcon = {
+    type: 'info-circle',
+    theme: 'twotone'
+  };
+  
+  submitForm(): void {
+    if (this.validateForm.valid) {
+      console.log('submit', this.validateForm.value);
+      this.modal.success({
+        nzTitle: 'Success Password Changed',
+        nzContent: 'Your password has been changed.',
+        nzOkText: "Ok"
+      });
+    } else {
+      Object.values(this.validateForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
+
+  updateConfirmValidator(): void {
+    /** wait for refresh value */
+    Promise.resolve().then(() => this.validateForm.controls['checkPassword'].updateValueAndValidity());
+  }
+
+  confirmationValidator = (control: FormControl): { [s: string]: boolean } => {
+    if (!control.value) {
+      return { required: true };
+    } else if (control.value !== this.validateForm.controls['newpassword'].value) {
+      return { confirm: true, error: true };
+    }
+    return {};
+  };
+
+  getCaptcha(e: MouseEvent): void {
+    e.preventDefault();
+  }
+
+  constructor(private fb: FormBuilder, private modal: NzModalService) {}
 
   ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      newpassword: ['123', [Validators.required]],
+      oldpassword: ['123', [Validators.required]],
+      checkPassword: [null, [Validators.required, this.confirmationValidator]],
+    });
   }
 
 }
