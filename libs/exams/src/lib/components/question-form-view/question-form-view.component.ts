e860f4d126b581fcbffb9cb05315ca9539fe1b1c
datienza1991 <datienza@doehle-mnl.com>
2022-04-03 07:43:27 +0800

    #81 | Update Question Form View UI

--- libs/exams/src/lib/components/question-form-view/question-form-view.component.ts
+++ libs/exams/src/lib/components/question-form-view/question-form-view.component.ts
@@ -1,20 +1,32 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { NzFormTooltipIcon } from 'ng-zorro-antd/form';
 import { NzModalService } from 'ng-zorro-antd/modal';
 import { Location } from '@angular/common';
 import { QuestionDetail } from '@batstateu/data-models';
+import { Editor, Toolbar } from 'ngx-editor';
 @Component({
   selector: 'batstateu-question-form-view',
   templateUrl: './question-form-view.component.html',
   styleUrls: ['./question-form-view.component.less']
 })
-export class QuestionFormViewComponent implements OnInit, OnChanges {
+export class QuestionFormViewComponent implements OnInit, OnChanges, OnDestroy {
+  editor!: Editor;
+  editorAns!: Editor;
   @Output() save = new EventEmitter<QuestionDetail>();
   @Input() questionDetail! : QuestionDetail;
   validateForm!: FormGroup;
   title = "Add New";
-  
+  toolbar: Toolbar = [
+    // default value
+    ['bold', 'italic' ],
+    ['underline', 'strike'],
+    ['blockquote'],
+    ['ordered_list', 'bullet_list'],
+    [{ heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
+    ['align_left', 'align_center', 'align_right', 'align_justify'],
+  ];
+
   submitForm(): void {
     if (this.validateForm.valid) {
       this.save.emit(this.validateForm.value);
@@ -48,8 +60,14 @@ export class QuestionFormViewComponent implements OnInit, OnChanges {
     private modal: NzModalService,
     private location: Location
   ) {}
+  ngOnDestroy(): void {
+    this.editor.destroy();
+    this.editorAns.destroy();
+  }
 
   ngOnInit(): void {
+    this.editor = new Editor();
+    this.editorAns = new Editor();
     this.validateForm = this.fb.group({
       question: [null,[Validators.required]],
       correctAnswer: [null,[Validators.required]],
