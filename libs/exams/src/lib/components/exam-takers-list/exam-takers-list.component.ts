5dfa82e879f1177585b2d21b3cbe77a2373ce6e2
datienza1991 <datienza@doehle-mnl.com>
2022-05-09 21:20:44 +0800

    #171 | Add Sorting of Exam Takers from A-Z and vise versa

--- libs/exams/src/lib/components/exam-takers-list/exam-takers-list.component.ts
+++ libs/exams/src/lib/components/exam-takers-list/exam-takers-list.component.ts
@@ -1,6 +1,13 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ExamTakerList } from '@batstateu/data-models';
+import { NzTableSortFn, NzTableSortOrder } from 'ng-zorro-antd/table';
 
+interface ColumnItem {
+  name: string;
+  sortOrder: NzTableSortOrder | null;
+  sortFn: NzTableSortFn<ExamTakerList> | null;
+  sortDirections: NzTableSortOrder[];
+}
 @Component({
   selector: 'batstateu-exam-takers-list',
   templateUrl: './exam-takers-list.component.html',
@@ -11,6 +18,15 @@ export class ExamTakersListComponent implements OnInit {
   @Output() viewScore = new EventEmitter();
   @Output() search = new EventEmitter<string>();
   searchText = '';
+
+  listOfColumns: ColumnItem[] = [
+    {
+      name: 'Name',
+      sortOrder: null,
+      sortFn: (a: ExamTakerList, b: ExamTakerList) => a.name.localeCompare(b.name),
+      sortDirections: ['ascend', 'descend', null]
+    }
+  ];
 
   onViewScore(userDetailId: number, examId: number){
     const takerExamIdObj = {userDetailId: userDetailId, examId: examId}
