119b9131c40d0469a58061f3d598c4199895c3e2
datienza1991 <datienza@doehle-mnl.com>
2022-05-28 19:50:17 +0800

    #175 | Instruction Grey text and removed bulleted text

--- libs/exams/src/lib/components/exam-form-view/exam-form-view.component.ts
+++ libs/exams/src/lib/components/exam-form-view/exam-form-view.component.ts
@@ -35,14 +35,32 @@ export class ExamFormViewComponent implements OnInit, OnChanges, OnDestroy {
   @Input() examDetail!: Exam;
   validateForm!: FormGroup;
   title = 'Add New';
-  instruction = `<h3><strong>Subject</strong>:</h3><h1><strong><u>Important</u></strong></h1><p>
-    Your Upcoming Exam The upcoming [Midterm/Final/Ect.] exam is facilitated using an online examination administration system.
-    </p><p>In order to take the exam, there are some important steps you will need to take.
-    </p><ul><li><p>Failure to do so in a timely manner may result in your not having access to the exam.
-    </p></li></ul><ul><li><p>Choose the entire screen when you share the screen.
-    </p></li><li><p>The application will request to access your camera and audio.
-    </p></li><li><p>The examination has a time limit set by the faculty.
-    </p></li><li><p>Leaving your exam tab for 15 seconds will end your examination automatically.</p></li></ul>`;
+  instruction = `<div style="color:#808080"><h3><strong>Subject</strong>:</h3>
+  <h1><strong><u>Important</u></strong></h1>
+  <p>
+     Your Upcoming Exam The upcoming [Midterm/Final/Ect.] exam is facilitated using an online examination administration system.
+  </p>
+  <p>In order to take the exam, there are some important steps you will need to take.
+  </p>
+  <p>Failure to do so in a timely manner may result in your not having access to the exam.
+  </p>
+  <ul>
+     <li>
+        <p>Choose the entire screen when you share the screen.
+        </p>
+     </li>
+     <li>
+        <p>The application will request to access your camera and audio.
+        </p>
+     </li>
+     <li>
+        <p>The examination has a time limit set by the faculty.
+        </p>
+     </li>
+     <li>
+        <p>Leaving your exam tab for 15 seconds will end your examination automatically.</p>
+     </li>
+  </ul></div>`;
 
   toolbar: Toolbar = [
     // default value
